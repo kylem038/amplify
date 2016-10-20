517a7c03fe782ca5f9fc8e880d89f602835f9287
kylem038 <misenckj@gmail.com>
2016-10-20 16:01:58 -0600

    Sass working correctly

---
+++ amplify/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Amplify</h1>
+          <button className='FacebookLogin' alt="Sign up with Facebook"></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- amplify/src/index.js
+++ amplify/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+require("!style!css!sass!./style/index.scss");
 
 ReactDOM.render(
   <App />,
