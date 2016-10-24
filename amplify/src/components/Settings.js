f5c4dad687c2a91129226b66863be4615c02f67e
kylem038 <misenckj@gmail.com>
2016-10-24 15:33:36 -0600

    Cleaned up readme, index files and added profile page

--- amplify/src/components/Settings.js
+++ amplify/src/components/Settings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function () {
+const Settings = () => {
   return (
     <section className='Settings'>
       <h1>Settings</h1>
@@ -15,3 +15,5 @@ export default function () {
     </section>
   );
 }
+
+export default Settings;
