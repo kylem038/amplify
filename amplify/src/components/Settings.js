1cd68b1d3282a0c1b1b88d6d78bc671f0f443bcc
kylem038 <misenckj@gmail.com>
2016-10-24 14:55:14 -0600

    Bandmates page added & CSS reset

--- amplify/src/components/Settings.js
+++ amplify/src/components/Settings.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export default function () {
   return (
     <section className='Settings'>
-      
+      <h1>Settings</h1>
+      <p>Instrument(s) You Play</p>
+      {/* drop down here? */}
+      <p>Genres You Enjoy</p>
+      {/* drop down here? */}
+      <p>Skill Level</p>
+      {/* drop down here? */}
+      <button className='SubmitSettings'>Submit</button>
+      <button className='CancelSettings'>Cancel</button>
     </section>
   );
 }
