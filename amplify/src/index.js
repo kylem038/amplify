f3505a43d671b0530034c1d77c7b3454eca363a0
kylem038 <misenckj@gmail.com>
2016-10-29 10:49:02 -0600

    Redux setup complete

--- amplify/src/index.js
+++ amplify/src/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import Settings from './components/Settings';
-import Instruments from './components/Instruments';
-import Genres from './components/Genres';
-import Skill from './components/Skill';
-import Dashboard from './components/Dashboard';
-import Profile from './components/Profile';
-import NotFound from './components/NotFound';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import Routes from './routes';
+import { Provider } from 'react-redux';
+import store from './store';
 require("!style!css!sass!./style/index.scss");
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Match exactly pattern='/' component={App} />
-      <Match exactly pattern='/settings' component={Settings} />
-      <Match exactly pattern='/instruments' component={Instruments} />
-      <Match exactly pattern='/genres' component={Genres} />
-      <Match exactly pattern='/skill' component={Skill} />
-      <Match exactly pattern='/dashboard' component={Dashboard} />
-      <Match exactly pattern='/profile' component={Profile} />
-      <Miss component={NotFound} />
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <Routes />
+  </Provider>
   ,
   document.getElementById('root')
 );
--- amplify/src/tests/integration/App.js
+++ amplify/src/tests/integration/App.js
@@ -1,19 +1,32 @@
 var Nightmare = require('nightmare');
 
-var expect = require('chai').expect;
+var chai = require('chai');
+// var asPromised = require('chai-as-promised');
+
+// chai.use(asPromised);
+
+var FacebookLogin = 'button.FacebookLogin';
 
 describe('log in page', function() {
-  it('should check if the log in button exists', function() {
-    var nightmare = Nightmare();
+  it('should check if the log in button exists', function(done) {
+    var nightmare = Nightmare({ show: true });
+    // var buttonExists = nightmare
+    //   .goto('http://localhost:3000/')
+    //   .exists(FacebookLogin)
+    //   .end();
+    // chai.expect(buttonExists).to.be.true;
+    // done();
+    // // buttonExists.should.be.true;
 
     nightmare
-      .goto('http://localhost:3000')
+      .goto('http://localhost:3000/')
+      .wait(FacebookLogin)
       .evaluate(function () {
-        return document.querySelector('button.FacebookLogin');
+        return document.querySelector(FacebookLogin);
       })
       .end()
       .then(function(button) {
-        expect(button.alt).to.equal('Sign up with Facebook');
+        chai.expect(button.alt).to.equal('Sign up with Facebook');
         done();
       });
   });
