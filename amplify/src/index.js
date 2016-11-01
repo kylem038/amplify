0e787e40b6c45addec3010f95a30c8a00988d779
kylem038 <misenckj@gmail.com>
2016-11-01 13:23:47 -0600

    Listening to auth on load

--- amplify/src/index.js
+++ amplify/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
 import { Provider } from 'react-redux';
+
+import Routes from './routes';
 import store from './store';
+import * as actions from './actions/auth';
+
 require("!style!css!sass!./style/index.scss");
 
 ReactDOM.render(
@@ -12,3 +15,5 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
+
+store.dispatch(actions.startListeningToAuth());
