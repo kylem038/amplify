af0bd28d6b142512f6515947361072be39c9ea68
kylem038 <misenckj@gmail.com>
2016-11-09 07:05:57 -0700

    Mapping through instruments

--- amplify/src/components/BandMateCard.js
+++ amplify/src/components/BandMateCard.js
@@ -5,7 +5,9 @@ const BandMateCard = (props) => {
   return(
     <section className="Bandmate">
       <p className='BandmateName'>{username}</p>
-      <p className='BandmateInstrument'>{instruments[0]}</p>
+      <span className='BandmateInstrument'>
+        {instruments.map((instrument, i) => <p key={i} children={instrument} />)}
+      </span>
       <span className='BandmateGenre'>
         {genres.map((g, i) => <p key={i} children={g} />)}
       </span>
